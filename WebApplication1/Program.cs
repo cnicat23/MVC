1aa5d1c0ecebb96295a5e9fc1183dd94c137129b
Nicat <cnicat2323@gmail.com>
2024-05-07 02:37:50 +0400

    MVC

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,19 @@
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+
+            app.MapControllerRoute("default", "{controller=home}/{action=index}");
+
+            app.Run();
+        }
+    }
+}
